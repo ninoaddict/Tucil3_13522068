6c89543ac81eb7019469474161d7a692f28d2fb5
ninoaddict <adrilbless37@gmail.com>
2024-05-04 05:19:44 +0700

    feat: refactor dictionary

--- src/word_ladder/DictionaryReader.java
+++ src/word_ladder/DictionaryReader.java
@@ -9,7 +9,7 @@ public class DictionaryReader {
   public static HashSet<String> ReadDictionary() throws IOException {
     HashSet<String> set = new HashSet<>();
 
-    final String fileName = "src/word_ladder/dictionary.txt";
+    final String fileName = "resource/dictionary.txt";
     BufferedReader reader = new BufferedReader(new FileReader(fileName));
 
     String word;
